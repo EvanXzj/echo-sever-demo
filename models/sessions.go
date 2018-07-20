63d9e393696395dda983b64ac511e6076d4c4f92
EvanXzj <2686465170@qq.com>
2018-07-20 23:13:15 +0800

    add sessions model

---
+++ models/sessions.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres" // for gorm
+)
+
+// Session : for user sessions
+type Session struct {
+	Token        string    `gorm:"primary_key;text" json:"token"`
+	UserID       uint      `gorm:"not null" json:"user_id"`
+	ClientIP     string    `gorm:"text" json:"client_ip"`
+	LastActivity time.Time `json:"last_activity"`
+}
+
+// SessionAPI : for interacting with session models
+type SessionAPI struct {
+	db *gorm.DB
+}
+
+// Create : create a user session in the db
+func (sess SessionAPI) Create(user User, token, client string) {
+	sess.db.Create(&Session{
+		UserID: user.ID,
+		Token: token,
+		ClientIP: client,
+		LastActivity: time.Now()
+	})
+}
+
+// ByToken : get a session by its token
+func (sess SessionAPI) ByToken(token uint) Session {
+	var session Session
+
+	sess.db.Find(&session, token)
+
+	return session
+}
+
+// Activity :  update a sessions last_activity
+func (sess SessionAPI) Activity(s *Session) {
+	sess.db.Update(&s, "last_activity", time.Now())
+}
