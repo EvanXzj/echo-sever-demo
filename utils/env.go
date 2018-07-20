1eb0e5e1c4613145cce8734c75caf77e233260a0
EvanXzj <2686465170@qq.com>
2018-07-20 14:00:53 +0800

    utils add Getenv help function

---
+++ utils/env.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"os"
+)
+
+// GetEnv : get an environment variable or its default value
+func GetEnv(k string) string {
+
+	env := os.Getenv(k)
+
+	// default fallbacks
+	if len(env) == 0 {
+		switch k {
+		case "APP_PORT":
+			return "7447"
+		case "JWT_SECRET":
+			return "verygoodsecret"
+		default:
+			panic("unrecognized env variable")
+		}
+	}
+
+	return env
+}
