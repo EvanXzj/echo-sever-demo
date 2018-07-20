15bd2fae6f9f9eb996796540cb0f7454d248f4af
EvanXzj <2686465170@qq.com>
2018-07-20 14:28:22 +0800

    utils add response helper function

---
+++ utils/response.go
@@ -0,0 +1,22 @@
+package utils
+
+// Response struct
+type Response struct {
+	Payload map[string](interface{})
+}
+
+// NewResponse : create a new response
+func NewResponse(k string, v interface{}) Response {
+
+	resp := Response{(map[string](interface{}){})}
+	resp.Payload[k] = v
+
+	return resp
+}
+
+// Set : add a new key/value to the reponses payload
+func (r *Response) Set(k string, v interface{}) {
+
+	r.Payload[k] = v
+
+}
