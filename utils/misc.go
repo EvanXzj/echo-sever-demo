626fdab0de911f5e61a0326011d3dca843683d64
EvanXzj <2686465170@qq.com>
2018-07-20 14:20:40 +0800

    add ConvertID helper funciton

---
+++ utils/misc.go
@@ -0,0 +1,14 @@
+package utils
+
+import "strconv"
+
+// ConvertID : String to uint parser
+func ConvertID(sID string) (uint, error) {
+	id, err := strconv.ParseUint(sID, 10, 32)
+
+	if err != nil {
+		return uint(0), err
+	}
+
+	return uint(id), nil
+}
